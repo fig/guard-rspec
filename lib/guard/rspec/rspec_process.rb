dae5f9dcfe9c17ca67e70f8bc05a2523b253364a
phil <phil@envy-manjaro>
2019-02-13 04:04:25 +0000

    refactor

--- lib/guard/rspec/rspec_process.rb
+++ lib/guard/rspec/rspec_process.rb
@@ -24,9 +24,8 @@ module Guard
 
       # Returns true if there is an error AND examples are not run.
       def error_and_examples_not_run?
-        error = "error occurred outside of examples"
-        summary_regexp = /0 examples, 0 failures( \((\d+) #{error}\))?/
-        !!results.summary.match(summary_regexp) && exit_code == 2
+        summary_regexp = /\A0 examples, 0 failures, \d? error/
+        results.summary.match(summary_regexp) && exit_code == 2
       end
 
       private
